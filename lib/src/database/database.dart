abac87a3e59c9fe17c29dd15e96afbae44d9cc9a
kimiya zargari <kimiyazargari13@gmail.com>
2023-02-24 19:13:12 +0330

    add database functions

--- lib/src/database/database.dart
+++ lib/src/database/database.dart
@@ -1,3 +1,4 @@
+import 'package:dart_grpc_api/src/generated/eshop.pb.dart';
 import 'package:hive/hive.dart';
 
 class EshopDatabase {
@@ -9,5 +10,49 @@ class EshopDatabase {
       {'categories', 'products'}, // Names of your boxes
       path: './',
     );
+  }
+
+  static Future<Category?> getCategory(int id) async {
+    throw UnimplementedError();
+  }
+
+  static Future<Product?> getProduct(int id) async {
+    throw UnimplementedError();
+  }
+
+  static Future<Category> addCategory(Category category) async {
+    throw UnimplementedError();
+  }
+
+  static Future<Product> addProduct(Product product) async {
+    throw UnimplementedError();
+  }
+
+  static Future<Category> editCategory(Category category) async {
+    throw UnimplementedError();
+  }
+
+  static Future<Product> editProduct(Product product) async {
+    throw UnimplementedError();
+  }
+
+  static Future<Empty> deleteCategory(int id) async {
+    throw UnimplementedError();
+  }
+
+  static Future<Empty> deleteProduct(int id) async {
+    throw UnimplementedError();
+  }
+
+  static Future<List<Product>?> getProductsOfCategory(int id) async {
+    throw UnimplementedError();
+  }
+
+  static Future<List<Category>> getAllCategories() async {
+    throw UnimplementedError();
+  }
+
+  static Future<List<Product>> getAllProducts() async {
+    throw UnimplementedError();
   }
 }
